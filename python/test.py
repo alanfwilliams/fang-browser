6e426911c7ccc97eba352110775e25db3d78e8a4
Alan Williams <astropiloto@gmail.com>
2025-02-15 20:12:45 -0600

    cleaning up warnings

--- python/test.py
+++ python/test.py
@@ -1,3 +1,3 @@
-import networking
+import networking  # type: ignore
 
-net_request("example.com", 80, "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n")
+networking.net_request("example.com", 80, "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n")
